567fcb5b80c359c4b5879e37b322fde1c1d06a23
Yago Mackay <yagomackay@outlook.com>
2023-03-09 23:45:27 +1100

    fix styling layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,59 @@
 import { Box, SimpleGrid, Text } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/react'
+import { Image, Skeleton } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
 import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
 
 const Home = ({ artists }) => {
-  const { user } = useMe()
+  const { user, isLoading } = useMe()
+
+  console.log('user', user)
 
   return (
-    <GradientLayout
-      roundImage
-      color="gray"
-      subtitle="profile"
-      title={`${user?.firstName} ${user?.lastName}`}
-      description={`${user?.playlistsCount} public playlists`}
-      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
-    >
-      <Box color="white" paddingX="40px">
-        <Box marginBottom="40px">
-          <Text fontSize="2xl" fontWeight="bold">
-            Top artist this month
-          </Text>
-          <Text fontSize="md">only visible to you</Text>
-        </Box>
-        <SimpleGrid minChildWidth="200px" spacing="20px">
-          {artists.map((artist) => (
-            <Box width="100%">
-              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
-                <Image src={artist.avatar} borderRadius="100%" />
-                <Box marginTop="20px">
-                  <Text fontSize="large">{artist.name}</Text>
-                  <Text fontSize="x-small">Artist</Text>
-                </Box>
-              </Box>
+    <Box>
+      <Skeleton isLoaded={!isLoading}>
+        <GradientLayout
+          roundImage
+          color="red"
+          subtitle="profile"
+          title={`${user?.firstName} ${user?.lastName}`}
+          description={`${user?.playlistsCount} public playlists`}
+          image="https://yago-mackay-profile.vercel.app/_next/image?url=%2Fimages%2Fprofile.jpg&w=256&q=75"
+        >
+          <Box color="white" paddingX="40px">
+            <Box marginBottom="40px">
+              <Text fontSize="2xl" fontWeight="bold">
+                Top artist this month
+              </Text>
+              <Text fontSize="md">only visible to you</Text>
             </Box>
-          ))}
-        </SimpleGrid>
-      </Box>
-    </GradientLayout>
+            <SimpleGrid minChildWidth="220px" spacing="40px">
+              {artists.map((artist) => (
+                <Box width="100%" height="300px">
+                  <Box
+                    bg="gray.900"
+                    borderRadius="4px"
+                    padding="15px"
+                    width="100%"
+                  >
+                    <Image
+                      src={artist.avatar}
+                      borderRadius="100%"
+                      height="200px"
+                      width="100%"
+                    />
+                    <Box marginTop="20px">
+                      <Text fontSize="large">{artist.name}</Text>
+                      <Text fontSize="x-small">Artist</Text>
+                    </Box>
+                  </Box>
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Box>
+        </GradientLayout>
+      </Skeleton>
+    </Box>
   )
 }
 
