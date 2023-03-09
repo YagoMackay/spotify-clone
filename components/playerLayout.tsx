c6c1f8211715f441dbc6581c365c48e3d73c1449
Yago Mackay <yagomackay@outlook.com>
2023-03-09 23:44:09 +1100

    fix playerLayout styling

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@chakra-ui/layout'
-import Sidebar from './sidebar'
 import PlayerBar from './playerBar'
+import Sidebar from './sidebar'
 
 const PlayerLayout = ({ children }) => {
   return (
-    <Box width="100vw" height="100vh">
-      <Box position="absolute" top="0" width="250px" left="0">
+    <Box width="100vw" height="100%">
+      <Box position="absolute" top="0" width="250px" left="0" height="100%">
         <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
